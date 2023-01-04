c5f48a0136ec268769d5475f204e5f4448d97797
Mykhail <movemens1992@gmail.com>
2023-01-04 18:11:20 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -18,18 +18,17 @@ const Reviews = lazy(() => import('./Reviews/Reviews'));
 export const App = () => {
   return (
     <>
-      <Header />
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
           <Route path="*" element={<Home />} />
-        </Routes>
-      </>
+        </Route>
+      </Routes>
 
       <GlobalStyles />
     </>
